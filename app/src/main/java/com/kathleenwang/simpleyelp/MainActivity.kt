c43eb43a76a2b85c26b57e75d8ae3da7941377bf
Kathleen Wang <kathleenchibimaru@gmail.com>
2020-12-29 11:38:54 -0800

    first

---
+++ app/src/main/java/com/kathleenwang/simpleyelp/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.kathleenwang.simpleyelp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+private const val BASE_URL = "https://api.yelp.com/v3/"
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+    }
+}
